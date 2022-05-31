8341b7dcfb094de8307285f507d92123120fae38
Patryk Ostrowski <patryk.ostrowski@int.pl>
2022-05-31 19:13:01 +0200

    Changing some tags in HTML

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 const start__button = document.querySelector("button");
 const start__block = document.getElementsByClassName("start")[0];
 const game__block = document.getElementsByClassName("body__block--game")[0];
+const roll__items__table = [];
+
 start__button.addEventListener("click", () => {
   start__block.style.display = "none";
   game__block.style.display = "flex";
 })
-
-class NewGame {
-  GameBlockInitialization = () => {
-    console.log(center__block__game);
-    center__block.appendChild(center__block__game);
-  }
-}
-
-game = new NewGame();
